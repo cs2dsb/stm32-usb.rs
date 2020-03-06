ee42becb97b08e9bbbae8c40b8f4982b8949ed59
cs2dsb <cs2dsb@gmail.com>
2020-03-06 00:51:36 +0000

    Some experimentation around binary size. Can't get below 21k because of fmt/panic machinery even with panic=abort and a panic handler that doesn't use the PanicInfo. Couldn't trigger any of the fmt code via breakpoints and running through all supported operations.

--- firmware/usbd_scsi/src/scsi/enums/additional_sense_code.rs
+++ firmware/usbd_scsi/src/scsi/enums/additional_sense_code.rs
@@ -1,7 +1,8 @@
 use codegen::asc_list_to_enum;
 
 asc_list_to_enum!{
-    pub AdditionalSenseCode = "src/scsi/enums/asc-num.txt";
+    //pub AdditionalSenseCode = "src/scsi/enums/asc-num.txt";
+    pub AdditionalSenseCode = "src/scsi/enums/asc-num_trimmed.txt";
 }           
 
 impl Default for AdditionalSenseCode {
